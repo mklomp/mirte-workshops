c6f706302c18943dc06db70f0a44db6cc3338e9a
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-07-29 12:22:30 +0200

    Add failing check_articles

--- scripts/check_articles.py
+++ scripts/check_articles.py
@@ -9,7 +9,7 @@ with open('_static/js/articles.json') as file:
 import os
 script_path = os.path.dirname(os.path.realpath(__file__))
 root = os.path.join(script_path, '..')
-
+err_count = 0
 # for every article and every language, check that the file exists
 for article in data['articles']:
     for lang in data['languages']:
@@ -20,4 +20,9 @@ for article in data['articles']:
                 pass
         except FileNotFoundError:
             print(f'::error title=::{root}/docs/{article}/{article}.{lang["short"]}.rst missing')
-            # exit(1)
+            err_count += 1
+
+
+if err_count > 0:
+    print(f'::error title=::{err_count} missing files')
+    exit(1)
