6f853f39ba61a69e8a1097a39669e7cfd644fbd1
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-07-29 13:07:27 +0200

    fix output multiline

--- scripts/check_changed_articles.py
+++ scripts/check_changed_articles.py
@@ -9,14 +9,14 @@ with open('_static/js/articles.json') as file:
 
 changed_files = sys.argv[1] 
 changed_files = changed_files.split(" ")
-print("Changed files: " + str(changed_files))
+# print("Changed files: " + str(changed_files))
 
 # for every article, check that if one of the files is changed, all are changed
 for article in data['articles']:
     article_files = []
     for lang in data['languages']:
         article_files.append(f'docs/{article}/{article}.{lang["short"]}.rst')
-    print("Article files: " + str(article_files))
+    # print("Article files: " + str(article_files))
     are_changed = [file in changed_files for file in article_files]
 
     if any(are_changed) and not all(are_changed):
